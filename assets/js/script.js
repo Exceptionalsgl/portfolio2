e7a839617d8edaab634f46aa6cc8c6dd904ac0ca
Precious O. Ayo <exceptionalsgl@gmail.com>
2024-06-15 06:01:32 +0000

    feat:add dom and event listener to my javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 
+// Dom element for quiz section
+const usernameSection = document.getElementById('username-section');
+const quizSection = document.getElementById('quiz-section');
+const resultSection = document.getElementById('result-section');
+const questionElement = document.getElementById('question');
+const choicesElement = document.getElementById('choices');
+const feedbackElement = document.getElementById('feedback');
+const scoreElement = document.getElementById('score');
+const finalScoreElement = document.getElementById('final-score');
+const startQuizButton = document.getElementById('start-quiz');
+const nextQuestionButton = document.getElementById('next-question');
+const restartQuizButton = document.getElementById('restart-quiz');
+const greetingElement = document.getElementById('greeting');
+
+
+// Event listeners for click buttons
+startQuizButton.addEventListener('click', startQuiz);
+nextQuestionButton.addEventListener('click', showNextQuestion);
+restartQuizButton.addEventListener('click', restartQuiz);
+
 function startQuiz() {
    
 }
